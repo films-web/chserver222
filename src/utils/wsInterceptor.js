3ba46b73cf6f70e40f3e9f95c03b2899f27df1fa
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-23 17:58:30 +0200

    Revert "debug"
    This reverts commit 381393d9a8450abf58b9b6ffdec8ce588b01b47d.

--- src/utils/wsInterceptor.js
+++ src/utils/wsInterceptor.js
@@ -2,19 +2,30 @@ function attachWsInterceptor(fastify, connection, clientId) {
     const originalSend = connection.socket.send.bind(connection.socket);
 
     connection.sendSuccess = (action, data = null) => {
-        const response = { action: action, status: 'success', data: data };
-        fastify.log.info(`[WS SEND -> Client ${clientId || 'Unauthed'}] Action: ${action} | Data: ${JSON.stringify(data)}`);
+        const response = {
+            action: action,
+            status: 'success',
+            data: data
+        };
         originalSend(JSON.stringify(response));
     };
 
     connection.sendError = (action, message) => {
-        fastify.log.error(`[WS ERROR -> Client ${clientId || 'Unauthed'}] Action: ${action} | ${message}`);
-        const response = { action: action, status: 'error', message: message || 'Internal Server Error' };
+        if (clientId) {
+            fastify.log.error(`[WS Error - Client ${clientId}] Action: ${action} | ${message}`);
+        } else {
+            fastify.log.error(`[WS Error - Unauthenticated] Action: ${action} | ${message}`);
+        }
+        
+        const response = {
+            action: action,
+            status: 'error',
+            message: message || 'Internal Server Error'
+        };
         originalSend(JSON.stringify(response));
     };
 
     connection.sendRawJSON = (payload) => {
-        fastify.log.info(`[WS RAW -> Client ${clientId || 'Unauthed'}] Payload: ${JSON.stringify(payload)}`);
         originalSend(JSON.stringify(payload));
     };
 }
