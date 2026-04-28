ad2231530b269d46f4313ab4e82f4de9d1a9315f
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 21:34:17 +0300

    Create protoloader.js

---
+++ src/utils/protoloader.js
@@ -0,0 +1,25 @@
+const protobuf = require('protobufjs');
+const path = require('path');
+const fs = require('fs');
+
+const protoPath = path.join(__dirname, '../../proto/message.proto');
+
+let C2SMessage;
+let S2CMessage;
+
+try {
+    const protoString = fs.readFileSync(protoPath, 'utf8');
+
+    const root = new protobuf.Root();
+    root.parse(protoString, { keepCase: true });
+
+    C2SMessage = root.lookupType("CheatHaram.C2S_Message");
+    S2CMessage = root.lookupType("CheatHaram.S2C_Message");
+
+    console.log('[PROTO] Schema loaded successfully with keepCase: true');
+} catch (err) {
+    console.error('[PROTO ERROR] Failed to load/parse Protobuf schema:', err.message);
+    process.exit(1);
+}
+
+module.exports = { C2SMessage, S2CMessage };
