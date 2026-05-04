c48495d5678144f161cf710ae077abe1c50cc76c
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-05-04 16:31:23 +0300

    Update index.js

--- src/routes/ws/handlers/index.js
+++ src/routes/ws/handlers/index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  AUTH_REQUEST: require('./auth'),
-  HEARTBEAT: require('./heartbeat'),
-  UPDATE_PLAYER_STATE: require('./updateState'),
-  PK3_WHITELIST_REQ: require('./requestWhitelist'),
-  PAYLOAD_REQ: require('./requestPayload'),
-  PLAYER_LIST_REQ: require('./requestAcStatus'),
-  GET_GUID_REQ: require('./requestGuid'),
-  REQUEST_FAIRSHOT: require('./requestFairshot'),
-  TAKE_FAIRSHOT: require('./takeFairshot'),
-  DISCONNECT: require('./disconnect')
+  handleAuth: require('./auth'),
+  handleHeartbeat: require('./heartbeat'),
+  handleUpdateState: require('./updateState'),
+  handleDisconnect: require('./disconnect'),
+  handleRequestWhitelist: require('./requestWhitelist'),
+  handleRequestPayload: require('./requestPayload'),
+  handleRequestAcStatus: require('./requestAcStatus'),
+  handleRequestGuid: require('./requestGuid'),
+  handleRequestFairshot: require('./requestFairshot'),
+  handleTakeFairshot: require('./takeFairshot')
 };
