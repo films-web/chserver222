f473a1c7dcab2b2e2d8b5621093915fed4da5408
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-05-07 13:37:06 +0300

    yes

--- src/routes/ws/protoloader.js
+++ src/routes/ws/protoloader.js
@@ -1,17 +1,18 @@
 const protobuf = require('protobufjs');
 const path = require('path');
-const protoJSON = require('./proto/messages.json');
 
 let C2SMessage, S2CMessage;
 
 try {
-    const root = protobuf.Root.fromJSON(protoJSON);
+    const protoPath = path.join(__dirname, 'proto/messages.proto');
+    const root = protobuf.loadSync(protoPath);
+    
     C2SMessage = root.lookupType("CheatHaram.C2S_Message");
     S2CMessage = root.lookupType("CheatHaram.S2C_Message");
 
-    console.log('[PROTO] Schema loaded from static JSON.');
+    console.log('[PROTO] Schema loaded from messages.proto');
 } catch (err) {
-    console.error('[PROTO ERROR] Failed to load JSON schema:', err.message);
+    console.error('[PROTO ERROR] Failed to load messages.proto:', err.message);
     process.exit(1);
 }
 
