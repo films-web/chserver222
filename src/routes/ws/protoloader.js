d7cb1b15f1483fa7216ac9eb015abe017a8b7030
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-05-07 13:38:24 +0300

    Revert "yes"
    This reverts commit f473a1c7dcab2b2e2d8b5621093915fed4da5408.

--- src/routes/ws/protoloader.js
+++ src/routes/ws/protoloader.js
@@ -1,18 +1,17 @@
 const protobuf = require('protobufjs');
 const path = require('path');
+const protoJSON = require('./proto/messages.json');
 
 let C2SMessage, S2CMessage;
 
 try {
-    const protoPath = path.join(__dirname, 'proto/messages.proto');
-    const root = protobuf.loadSync(protoPath);
-    
+    const root = protobuf.Root.fromJSON(protoJSON);
     C2SMessage = root.lookupType("CheatHaram.C2S_Message");
     S2CMessage = root.lookupType("CheatHaram.S2C_Message");
 
-    console.log('[PROTO] Schema loaded from messages.proto');
+    console.log('[PROTO] Schema loaded from static JSON.');
 } catch (err) {
-    console.error('[PROTO ERROR] Failed to load messages.proto:', err.message);
+    console.error('[PROTO ERROR] Failed to load JSON schema:', err.message);
     process.exit(1);
 }
 
